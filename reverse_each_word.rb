13fda9596fff0f17498991677b97cc2043e26621
Latisha Joe <latisha_joe@yahoo.com>
2020-09-16 11:06:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,16 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each do|string|
-    new_array << string.reverse
+   new_array << string.reverse
  end
  new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ") 
+  new_array = []
+  array.collect do|string| 
+    new_array << string.reverse
+  end 
+end
+    
